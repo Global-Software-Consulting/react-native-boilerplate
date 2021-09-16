f1400bff4f3d2c062134642df89488ca37a0f834
ahtisham <ahtisham@gsoftconsulting.com>
2021-09-16 23:20:30 +0500

    feat: integrate react navigation and redux

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
  */
 import 'react-native-gesture-handler';
 
+import App from 'app/EntryPoint';
 import { AppRegistry } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 
 import { name as appName } from './app.json';
-import App from './app/Entrypoint';
 
 enableScreens();
 
