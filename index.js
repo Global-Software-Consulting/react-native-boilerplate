6b7eb25fc7fa7a2fe732db87dcbf4e5f20706f43
ahtisham <ahtisham@gsoftconsulting.com>
2021-09-23 15:45:31 +0500

    refactor: refector folder structure

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import 'react-native-gesture-handler';
 import App from 'app/EntryPoint';
 import { AppRegistry } from 'react-native';
 import { enableScreens } from 'react-native-screens';
-import './app/lanuages/index';
+import './app/languages/index';
 import { name as appName } from './app.json';
 
 enableScreens();
