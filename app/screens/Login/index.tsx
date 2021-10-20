a964f650353a2ecbba24fa0be08e304baa0f5fbf
Gsoft02 <gsoft02@Gsoft02s-MacBook-Pro.local>
2021-10-20 10:19:46 +0500

    Animations added to logins screen

--- app/screens/Login/index.tsx
+++ app/screens/Login/index.tsx
@@ -1,11 +1,12 @@
 import StdButton from 'app/components/StandardButton/index';
 import NavigationService from 'app/navigation/NavigationService';
 import * as loginActions from 'app/store/actions/loginActions';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Animated, Image, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useStyle } from './styles';
 import { useTheme } from 'react-native-paper';
+import { transform } from '@babel/core';
 const Login: React.FC = () => {
     // const id = useSelector((state: RootState) => state.user.id);
     const dispatch = useDispatch();
@@ -13,8 +14,45 @@ const Login: React.FC = () => {
     const theme = useTheme();
     const onLogin = () => dispatch(loginActions.requestLogin('test', '1234'));
     const onForgot = () => NavigationService.navigate('ForgotPassword');
+    const rotation = new Animated.Value(0);
+    const rotationValue = rotation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
+    const opacityValue = new Animated.Value(0);
+
+    const [showText, setShowText] = useState(false);
+    useEffect(() => {
+        Animated.timing(rotation, {
+            toValue: 1,
+            duration: 5000,
+            useNativeDriver: true,
+        }).start();
+        setTimeout(() => {
+            Animated.spring(opacityValue, {
+                toValue: 1,
+                useNativeDriver: true,
+            }).start();
+        }, 5000);
+    }, []);
+
     return (
         <View style={styles.container}>
+            <View style={styles.imageStyle}>
+                <Animated.Image
+                    source={require('app/assets/react-native.png')}
+                    style={{ transform: [{ rotate: rotationValue }] }}
+                />
+            </View>
+            <Animated.Text
+                style={{
+                    fontSize: 20,
+                    color: 'teal',
+                    fontWeight: 'bold',
+                    opacity: opacityValue,
+                }}>
+                GSoft Boiler Plate
+            </Animated.Text>
             <View style={styles.subContainer}>
                 <StdButton
                     title="Login"
--- app/screens/Login/styles.ts
+++ app/screens/Login/styles.ts
@@ -1,27 +1,38 @@
 import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
-import React from 'react';
+import React, { ReducerState } from 'react';
 import { useSelector } from 'react-redux';
+import { RootState } from 'app/store/slice';
 export const useStyle = () => {
     const { colors } = useTheme();
-    const isDark = useSelector((state) => state.theme.isDark);
-    console.log('isDarkisDark', isDark);
+    const isDark = useSelector((state:RootState) => state.theme.isDark);
     const styles = () =>
         StyleSheet.create({
             container: {
                 flex: 1,
-                justifyContent: 'center',
                 alignItems: 'center',
+                paddingTop: '20%',
                 backgroundColor: colors.background,
             },
             subContainer: {
+                marginTop: '10%',
                 backgroundColor: colors.background,
                 borderRadius: 20,
                 alignItems: 'center',
                 borderWidth: 2,
                 padding: 20,
                 borderColor: isDark ? 'teal' : 'black',
+                shadowColor: "#000",
+                shadowOffset: {
+                    width: 0,
+                    height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                
+                elevation: 5,
             },
+          
             login: {
                 padding: 8,
             },
